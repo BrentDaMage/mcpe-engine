28ae251fb857c5cb20f4725cf160e48a2c28d0e9
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2025-10-16 23:51:30 -0500

    Remove TexturePtr

--- src-deps/Renderer/HAL/Base/TextureBase.h
+++ src-deps/Renderer/HAL/Base/TextureBase.h
@@ -4,21 +4,19 @@
 
 namespace mce
 {
+    class TextureBase
+    {
+    public:
+        TextureDescription m_description;
 
-class TextureBase
-{
-public:
-    TextureDescription m_description;
+    private:
+        bool m_created;
 
-private:
-    bool m_created;
-
-public:
-    void getDescription() { }
-    void deleteTexture() { m_created = false; }
-    bool isLoaded() { return m_created; }
-    void createTexture(const TextureDescription& description) { m_description = description; }
-    void convertToMipmapedTexture(unsigned int mipmaps);
-};
-
+    public:
+        void getDescription() { }
+        void deleteTexture() { m_created = false; }
+        bool isLoaded() { return m_created; }
+        void createTexture(const TextureDescription& description) { m_description = description; }
+        void convertToMipmapedTexture(unsigned int mipmaps);
+    };
 }
