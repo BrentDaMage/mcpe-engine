10317246af10728e859b264f525a927e583742ef
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2025-10-23 18:43:48 -0500

    Implemented ShaderBase

--- src-deps/Renderer/HAL/Base/ShaderBase.cpp
+++ src-deps/Renderer/HAL/Base/ShaderBase.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include "ShaderBase.h"
 
 using namespace mce;
@@ -8,4 +10,37 @@ ShaderBase::ShaderBase(ShaderProgram& vertexShader, ShaderProgram& fragmentShade
     , m_geometryShader(geometryShader)
 {
     m_attributeListIndex = 0;
+}
+
+bool ShaderBase::isBuiltFrom(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath, const std::string& geometryPath) const
+{
+    return m_vertexShader.m_header == (shaderName + vertexPath) &&
+           m_fragmentShader.m_header == (shaderName + fragmentPath) &&
+          (m_geometryShader.m_header == (shaderName + geometryPath) || !m_geometryShader.m_valid);
+}
+
+void ShaderBase::validateVertexFormat(const VertexFormat& vertexFormat)
+{
+}
+
+void ShaderBase::reflectShader(mce::RenderDevice& renderDevice)
+{
+}
+
+const mce::VertexField& ShaderBase::getAttributeForName(const std::string& name, unsigned int id) const
+{
+    std::map<std::string, VertexField>::iterator it = builtinAttributeMap.find(name);
+
+    if (it != builtinAttributeMap.end())
+    {
+        return it->second;
+    }
+    else
+    {
+        std::stringstream ss;
+        ss << name << id;
+        it = builtinAttributeMap.find(ss.str());
+
+        return it->second;
+    }
 }
--- src-deps/Renderer/HAL/Base/ShaderBase.h
+++ src-deps/Renderer/HAL/Base/ShaderBase.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 #include "ShaderProgram.h"
+#include "Attribute.h"
+#include "VertexFormat.h"
+#include "RenderDevice.h"
 
 namespace mce
 {
@@ -12,6 +18,13 @@ namespace mce
         ShaderProgram& m_fragmentShader;
         ShaderProgram& m_geometryShader;
 
+        static std::map<std::string, VertexField> builtinAttributeMap;
+
         ShaderBase(ShaderProgram& vertexShader, ShaderProgram& fragmentShader, ShaderProgram& geometryShader);
+
+        bool isBuiltFrom(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath, const std::string& geometryPath) const;
+        void validateVertexFormat(const VertexFormat& vertexFormat);
+        void reflectShader(RenderDevice& renderDevice);
+        const mce::VertexField& getAttributeForName(const std::string& name, unsigned int id) const;
     };
 }
