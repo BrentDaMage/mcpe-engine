9aa3267811de8efa66ad7d8062d965191da27ad2
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2025-10-24 01:44:04 -0500

    Implemented "gl::" Functions

--- src-deps/Renderer/HAL/OGL/API_OGL.h
+++ src-deps/Renderer/HAL/OGL/API_OGL.h
@@ -1,4 +1,20 @@
 #pragma once
 
+#include <string>
+
 #include <windows.h>
 #include <gl/GL.h>
+
+namespace gl
+{
+    extern bool hardwareOverideOpenGLES3;
+    
+    std::string getOpenGLVendor();
+    std::string getOpenGLRenderer();
+    std::string getOpenGLVersion();
+    unsigned int getOpenGLMajorVersion();
+    bool isOpenGLES3();
+    int getMaxVertexCount();
+    std::string getOpenGLExtensions();
+    bool supportsMipmaps();
+}
