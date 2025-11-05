e3dbcb8d0e262051992e02f41e6d53c40f01a77a
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2025-11-05 05:15:07 -0600

    Cleanup ShaderUniformOGL

--- src-deps/Renderer/HAL/OGL/ShaderUniformOGL.cpp
+++ src-deps/Renderer/HAL/OGL/ShaderUniformOGL.cpp
@@ -2,6 +2,16 @@
 
 using namespace mce;
 
+ShaderUniformOGL::ShaderUniformOGL(const std::string& name, int location, int elements, ShaderPrimitiveTypes shaderPrimitiveType)
+    : m_location(location)
+    , m_elements(elements)
+    , m_name(name)
+    , m_shaderPrimitiveType(shaderPrimitiveType)
+{
+    m_unknown = 0;
+    m_shaderConstant = nullptr;
+}
+
 void ShaderUniformOGL::bind(bool forceBind)
 {
     if (forceBind || m_shaderConstant->isDirty())
--- src-deps/Renderer/HAL/OGL/ShaderUniformOGL.h
+++ src-deps/Renderer/HAL/OGL/ShaderUniformOGL.h
@@ -1,33 +1,23 @@
 #include <string>
 
 #include "../Enums/ShaderPrimitiveTypes.h"
-#include "../Base/ShaderConstantBase.h"
 #include "ShaderConstantOGL.h"
 
 namespace mce
 {
+    class ShaderUniformOGL
+    {
+    public:
+        int m_location;
+        int m_elements;
+        ShaderPrimitiveTypes m_shaderPrimitiveType;
+        uint8_t m_unknown; // @TODO
+        std::string m_name;
+        ShaderConstantOGL* m_shaderConstant;
 
-class ShaderUniformOGL : public ShaderConstantBase
-{
-public:
-    int m_location;
-    int m_size;
-    ShaderPrimitiveTypes m_shaderPrimitiveType;
-    unsigned char byte9; // @TODO
-    std::string m_name;
-    ShaderConstantOGL* m_shaderConstant;
+    public:
+        ShaderUniformOGL(const std::string& name, int location, int elements, ShaderPrimitiveTypes shaderPrimitiveType);
 
-public:
-    ShaderUniformOGL(std::string name, int location, int size, ShaderPrimitiveTypes shaderPrimitiveType)
-        : m_location(location)
-        , m_size(size)
-        , byte9(0)
-        , m_shaderConstant(0)
-        , m_name(name)
-        , m_shaderPrimitiveType(shaderPrimitiveType)
-    {}
-
-    void bind(bool forceBind);
-};
-
+        void bind(bool forceBind);
+    };
 }
