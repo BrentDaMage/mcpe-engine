5480c4a72c63de64030228ef70139e3cc1bfb854
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2025-11-04 22:03:57 -0600

    Implemented InheritanceTree

---
+++ src-deps/Core/Utility/InheritanceTree.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+
+template <typename T>
+class InheritanceTree
+{
+public:
+    struct Node
+    {
+        std::string name;
+        T val;
+        std::vector<Node*> child;
+        bool isVisited;
+
+        Node(const std::string& name)
+        {
+            this->name = name;
+        }
+    };
+
+private:
+    std::map<const std::string, Node> m_nodes;
+
+public:
+    InheritanceTree() {}
+
+public:
+    Node& _node(const std::string& name)
+    {
+        std::map<const std::string, Node>::iterator it = m_nodes.find(name);
+        if (it != m_nodes.end())
+        {
+            return it->second;
+        }
+        else
+        {
+            Node& node = m_nodes[name];
+            node = Node(name);
+            return node;
+        }
+    }
+
+    void visitBFS(void (*callback)(const std::string&, const T&))
+    {
+        std::deque<Node*> queue;
+
+        queue.push_back(&m_nodes[""]);
+
+        while (!queue.empty())
+        {
+            Node* currentNode = queue.front();
+            queue.pop_front();
+
+            // Skip root node
+            if (!currentNode->name.empty())
+            {
+                if (!callback)
+                    throw std::bad_cast();
+
+                callback(currentNode->name, currentNode->val);
+            }
+
+            // Queue child nodes
+            for (int i = 0; i < currentNode->child.size(); i++)
+            {
+                queue.push_back(currentNode->child[i]);
+            }
+        }
+    }
+};
